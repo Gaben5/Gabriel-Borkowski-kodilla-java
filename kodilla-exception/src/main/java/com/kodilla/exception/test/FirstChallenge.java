4108eaaf4560ba9263c80b45028221d415901fc1
Gabriel Borkowski <gabriell.borkowski@gmail.com>
2023-02-25 15:24:07 +0100

    8.2 Additional task

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -7,6 +7,22 @@ public class FirstChallenge {
         }
         return a/b;
     }
+    public int power(int a, int b){
+        int pow = a;
+        int result = 0;
+        if (b == 0){
+            result = 1;
+        } else if (a == 0) {
+            result = 0;
+        } else if (b == 1) {
+            result = a;
+        } else {
+            for (int i = 0 ; i < b ; i++){
+               result = a *= pow;
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
@@ -19,5 +35,10 @@ public class FirstChallenge {
             System.out.println("result: "+result);
             System.out.println("Done");
         }
+
+        int resultPower1 = firstChallenge.power(0,5);
+        int resultPower2 = firstChallenge.power(5,0);
+        int resultPower3 = firstChallenge.power(5,1);
+
     }
 }
