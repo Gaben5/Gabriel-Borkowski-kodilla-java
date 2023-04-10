99a0af9e81cb65ca83217e62f336d2204be81210
Gabriel Borkowski <gabriell.borkowski@gmail.com>
2023-04-10 11:45:27 +0200

    14.1 Add ForumUser class and test with bean

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ForumTestSuite {
+    @Test
+            void testGetUsername() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String name = forumUser.getUsername();
+        //then
+        assertEquals("John Smith", name);
+    }
+}
